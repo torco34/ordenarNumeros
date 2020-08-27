28b891f90b28c78306c8dbd32c9d4dde2f555de5
torco34 <torcoromaarias@gimail.com>
2020-08-27 10:39:53 -0500

    Organizacion de numeros, primer algoritmo de meno a mayo, segundo de mayo a menor

--- folder.js/index.js
+++ folder.js/index.js
@@ -1,3 +1,48 @@
+function prinerOrden( ordenarArreglo, numero1, numero2){
+	if(numero1 === numero2){
+		return ordenenarArreglo;
+	}
+	let  cambio =  ordenarArreglo[numero1];
+
+	 ordenarArreglo[numero1] =  ordenarArreglo[numero2];
+	 ordenarArreglo[numero2] =  cambio;
+	 
+	return  ordenarrreglo;
+
+}
+
+
+
+
+
+ function  seleccionar(ordenDeArreglo){
+ 	let longitud = ordenDeArreglo.length;
+ 	for(let i =0; i<longitud -1; i++){
+
+ 		let numeroPequeno = i;
+ 		
+ 		for(let j = i + 1; j < longitud; j++ ){
+ 			if(ordenDeArreglo[j] < ordenDeArreglo[numeroPequeno]){
+ 				numeroPequeno = j;
+ 			}
+ 		}
+ 		prinerOrden(ordenDeArreglo, numeroPequeno, i);
+ 	}
+ 	return ordenDeArreglo;
+ }
+
+let ordenDeArreglo = [6,8,2,4,0,1,5,7];
+
+console.log(seleccionar(ordenDeArreglo));
+
+
+
+
+
+
+
+
+
 
 function intercabio(numerosAOrdenar, primerNu, segundoNu){
 	let ordenar = numerosAOrdenar[primerNu];
@@ -7,11 +52,11 @@ function intercabio(numerosAOrdenar, primerNu, segundoNu){
 }
 
 
-function organizarNumeros(numerosOrdenar){
+function organizarNumeros(numerosAOrdenar){
 	let seleccion = numerosAOrdenar.length;
 	for(let i=1; i<seleccion; i++){
 		for(let j = 0; j< (seleccion - i); j++){
-			let derecha = j + 1;
+			let derecha = j - 1;
 			if(numerosAOrdenar[j] > numerosAOrdenar[derecha]){
 				intercabio(numerosAOrdenar, j, derecha);
 
@@ -22,5 +67,5 @@ function organizarNumeros(numerosOrdenar){
 	
  return numerosAOrdenar
 }
-let numerosAOrdenar= [12,34,67,35,90,14,18,24,30,10]
+let numerosAOrdenar = [12,34,67,35,90,14,18,24,30,10]
 console.log(organizarNumeros ( numerosAOrdenar) )
