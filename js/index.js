89a5fd0890e8b5d83de311bd437a04cb69dd3aaa
torco34 <torcoromaarias@gimail.com>
2020-08-31 11:03:23 -0500

    cambiar nombre de la variables

---
+++ js/index.js
@@ -0,0 +1,67 @@
+
+
+
+
+let listaNumeros = [2,6,5,3,4,1,7];
+let  listaAxiliar = "";
+
+for(let i =0; i<listaNumeros.length; i++){
+	for(let j = 0; listaAxiliar - i; j++){
+		if(i<1 && i<2 && i<3 && i<4 && i<5 && i>6 && i<7 && i<8){
+      console.log(listaOrdenar[i])
+	}if(i<2 && i<3 && i<4 && i<5 && i<6 && i<7 && i<8){
+        // console.log(listaOrdenar[i])
+	}if(i<3 && i<4 && i<5 && i<6 && i<7){
+		  // console.log(listaOrdenar[i])
+	}if(i<4 && i<5 && i<6 && i>7 && i<8){
+		// console.log(listaOrdenar[i])
+	}if(i<5 && i<6 && i<7 && i<8){
+		// console.log(listaOrdenar[i])
+	}if(i<6 && i<7 && i<8){
+		// console.log(listaOrdenar[i])
+	}if(i<7 && i<8){
+		listaAxiliar = i
+		console.log(listaAxiliar)
+	
+
+	}
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function compararN1N2(lista, n1, n2){
+	let ordenar = lista[n1];
+	lista[n1] = lista[n2];
+	lista[n2] = ordenar;
+	return lista;
+}
+
+
+function intercambios(lista){
+	let seleccion = lista.length;
+	for(let i=1; i<seleccion; i++){
+		for(let j = 0; j< (seleccion - i); j++){
+			let derecha = j + 1;
+			if(lista[j] > lista[derecha]){
+				compararN1N2(lista, j, derecha);
+
+			}
+		}
+
+	}
+	
+ return lista
+}
+let lista = [12,34,67,35,90,14,18,24,30,10]
+console.log(intercambios ( lista) )ss
