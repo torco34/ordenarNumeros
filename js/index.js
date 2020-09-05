c1d671e41e6b681fe6c3cceba08a3f70768577cc
torco34 <torcoromaarias@gimail.com>
2020-09-05 14:39:27 -0500

    los numeros del arreglo se suman

--- js/index.js
+++ js/index.js
@@ -2,33 +2,33 @@
 
 
 
-let listaNumeros = [2,6,5,3,4,1,7];
-let  listaAxiliar = "";
+// let listaNumeros = [2,6,5,3,4,1,7];
+// let  listaAxiliar = "";
 
-for(let i =0; i<listaNumeros.length; i++){
-	for(let j = 0; listaAxiliar - i; j++){
-		if(i<1 && i<2 && i<3 && i<4 && i<5 && i>6 && i<7 && i<8){
-      console.log(listaOrdenar[i])
-	}if(i<2 && i<3 && i<4 && i<5 && i<6 && i<7 && i<8){
-        // console.log(listaOrdenar[i])
-	}if(i<3 && i<4 && i<5 && i<6 && i<7){
-		  // console.log(listaOrdenar[i])
-	}if(i<4 && i<5 && i<6 && i>7 && i<8){
-		// console.log(listaOrdenar[i])
-	}if(i<5 && i<6 && i<7 && i<8){
-		// console.log(listaOrdenar[i])
-	}if(i<6 && i<7 && i<8){
-		// console.log(listaOrdenar[i])
-	}if(i<7 && i<8){
-		listaAxiliar = i
-		console.log(listaAxiliar)
+// for(let i =0; i<listaNumeros.length; i++){
+// 	for(let j = 0; listaAxiliar - i; j++){
+// 		if(i<1 && i<2 && i<3 && i<4 && i<5 && i>6 && i<7 && i<8){
+//       console.log(listaOrdenar[i])
+// 	}if(i<2 && i<3 && i<4 && i<5 && i<6 && i<7 && i<8){
+//         // console.log(listaOrdenar[i])
+// 	}if(i<3 && i<4 && i<5 && i<6 && i<7 && i<8){
+// 		  // console.log(listaOrdenar[i])
+// 	}if(i<4 && i<5 && i<6 && i>7 && i<8){
+// 		// console.log(listaOrdenar[i])
+// 	}if(i<5 && i<6 && i<7 && i<8){
+// 		// console.log(listaOrdenar[i])
+// 	}if(i<6 && i<7 && i<8){
+// 		// console.log(listaOrdenar[i])
+// 	}if(i<7 && i<8){
+// 		listaAxiliar = i
+// 		console.log(listaAxiliar)
 	
 
-	}
-	}
+// 	}
+// 	}
 	
 
-}
+// }
 
 
 
@@ -40,28 +40,28 @@ for(let i =0; i<listaNumeros.length; i++){
 
 
 
-function compararN1N2(lista, n1, n2){
-	let ordenar = lista[n1];
-	lista[n1] = lista[n2];
-	lista[n2] = ordenar;
-	return lista;
-}
+// function compararN1N2(lista, n1, n2){
+// 	let ordenar = lista[n1];
+// 	lista[n1] = lista[n2];
+// 	lista[n2] = ordenar;
+// 	return lista;
+// }
 
 
-function intercambios(lista){
-	let seleccion = lista.length;
-	for(let i=1; i<seleccion; i++){
-		for(let j = 0; j< (seleccion - i); j++){
-			let derecha = j + 1;
-			if(lista[j] > lista[derecha]){
-				compararN1N2(lista, j, derecha);
+// function intercambios(lista){
+// 	let seleccion = lista.length;
+// 	for(let i=1; i<seleccion; i++){
+// 		for(let j = 0; j< (seleccion - i); j++){
+// 			let derecha = j + 1;
+// 			if(lista[j] > lista[derecha]){
+// 				compararN1N2(lista, j, derecha);
 
-			}
-		}
+// 			}
+// 		}
 
-	}
+// 	}
 	
- return lista
-}
-let lista = [12,34,67,35,90,14,18,24,30,10]
-console.log(intercambios ( lista) )ss
+//  return lista
+// }
+// let lista = [12,34,67,35,90,14,18,24,30,10]
+// console.log(intercambios ( lista) )ss
